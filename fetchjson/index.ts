58720de3c6418e19abc0acf6e6afaacfd9bcdfac
조성원 <tjddnjsjo@kakao.com>
2023-09-10 17:26:36 +0900

    fix argument order

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -15,10 +15,10 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
-  logTodo(id, completed, title);
+  logTodo(id, title, completed);
 });
 
-const logTodo = (id, title, completed) => {
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
