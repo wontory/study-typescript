feaa5f3270658e7aaa118243dc8840a65547e9f0
조성원 <tjddnjsjo@kakao.com>
2023-09-11 22:40:21 +0900

    TS Compile

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    logTodo(id, title, completed);
 });
+var logTodo = function (id, title, completed) {
+    console.log("\n    The Todo with ID: ".concat(id, "\n    Has a title of: ").concat(title, "\n    Is it finished? ").concat(completed, "\n  "));
+};
