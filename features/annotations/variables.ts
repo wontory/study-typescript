1c34599ac9036e16766fc26737c04ef42686f429
조성원 <tjddnjsjo@kakao.com>
2023-09-12 19:15:59 +0900

    타입 추론 이해하기

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,5 +1,12 @@
-let apples: number = 5;
-let speed: string = 'fast';
+// 변수 선언과 초기화가 동시에 이루어지면 타입 추론이 일어난다. (apples: number)
+let apples = 5;
+apples = 'delicious'; // apples의 타입이 number로 추론되었기 때문에 에러가 발생한다.
+
+// 동시에 이루어지지 않으면 타입 추론이 일어나지 않는다. (apples: any)
+let speed;
+speed = 'fast';
+speed = 10; // any 타입이기 때문에 에러가 발생하지 않는다.
+
 let hasName: boolean = true;
 
 let nothingMuch: null = null;
@@ -27,3 +34,8 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+//* 1) 변수 선언을 하고 초기화를 나중에 할 때
+//* 2) 추론이 불가능한 타입을 사용할 때
+//* 3) 함수가 any 타입을 반환하는데, 그 값을 명확히 하고 싶을 때
