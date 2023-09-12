74854f9028f77cbfe3e02f5fed2f5cf32c956d60
조성원 <tjddnjsjo@kakao.com>
2023-09-12 18:48:03 +0900

    Object literal annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -7,3 +7,18 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
